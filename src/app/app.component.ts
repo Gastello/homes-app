ec7ef302340d3b4c4490b87ef4709ffa303b5de5
Gastello <maximgst@gmail.com>
2024-12-18 00:13:35 +0200

    com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { HomeComponent } from "./home/home.component";
 import { RouterModule } from "@angular/router";
 
 @Component({
@@ -19,7 +18,7 @@ import { RouterModule } from "@angular/router";
     </section>
   </main>`,
   styleUrls: ["./app.component.css"],
-  imports: [HomeComponent, RouterModule],
+  imports: [RouterModule],
 })
 export class AppComponent {
   title = "homes";
